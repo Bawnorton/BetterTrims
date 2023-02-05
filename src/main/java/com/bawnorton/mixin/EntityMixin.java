811f121729b281ab833cf67fc3a5389a3ee50b93
Benjamin-Norton <bawnorton@me.com>
2023-02-04 19:42:15 -0800

    Fix fireblock damage bug

--- src/main/java/com/bawnorton/mixin/EntityMixin.java
+++ src/main/java/com/bawnorton/mixin/EntityMixin.java
@@ -3,38 +3,31 @@ package com.bawnorton.mixin;
 import com.bawnorton.BetterTrims;
 import com.bawnorton.effect.ArmorTrimEffects;
 import com.bawnorton.util.Wrapper;
-import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.minecraft.entity.Entity;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Entity.class)
 public abstract class EntityMixin {
     @Shadow
     public abstract Iterable<ItemStack> getArmorItems();
 
-    @ModifyReturnValue(method = "isFireImmune", at = @At("RETURN"))
-    private boolean isFireImmune(boolean original) {
+    @Inject(method = "isFireImmune", at = @At("RETURN"), cancellable = true)
+    private void isFireImmune(CallbackInfoReturnable<Boolean> cir) {
         Wrapper<Float> netheriteCount = Wrapper.of(0f);
         ArmorTrimEffects.NETHERITE.apply(getArmorItems(), stack -> netheriteCount.set(netheriteCount.get() + BetterTrims.CONFIG.netheriteFireResistance));
-        return original || netheriteCount.get() >= 0.99f;
+        cir.setReturnValue(cir.getReturnValue() ||  netheriteCount.get() >= 0.99f);
     }
 
     @ModifyArg(method = "setOnFireFromLava", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;damage(Lnet/minecraft/entity/damage/DamageSource;F)Z"))
-    private float setOnFireFromLava(float original) {
+    private float modifyDamage(float original) {
         Wrapper<Float> netheriteCount = Wrapper.of(0f);
         ArmorTrimEffects.NETHERITE.apply(getArmorItems(), stack -> netheriteCount.set(netheriteCount.get() + BetterTrims.CONFIG.netheriteFireResistance));
         return original * (1 - netheriteCount.get());
-    }
-
-    @ModifyArg(method = "setOnFireFor", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;setFireTicks(I)V"))
-    private int setFireTicks(int original) {
-        Wrapper<Float> netheriteCount = Wrapper.of(0f);
-        ArmorTrimEffects.NETHERITE.apply(getArmorItems(), stack -> netheriteCount.set(netheriteCount.get() + BetterTrims.CONFIG.netheriteFireResistance));
-        return (int) (original * (1 - netheriteCount.get()));
     }
 }
