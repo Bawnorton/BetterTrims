a2784d72c447ff14a42c58b0c8660ace0e52a2b1
Benjamin-Norton <bawnorton@me.com>
2023-07-29 11:59:54 -0400

    Add redstone trim support for elytra and horses

--- src/main/java/com/bawnorton/mixin/LivingEntityMixin.java
+++ src/main/java/com/bawnorton/mixin/LivingEntityMixin.java
@@ -3,15 +3,20 @@ package com.bawnorton.mixin;
 import com.bawnorton.config.Config;
 import com.bawnorton.effect.ArmorTrimEffects;
 import com.bawnorton.util.Wrapper;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.passive.AbstractHorseEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.Vec3d;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @SuppressWarnings("unused")
@@ -29,9 +34,32 @@ public abstract class LivingEntityMixin {
 
     @Inject(method = "getMovementSpeed()F", at = @At("RETURN"), cancellable = true)
     private void modifyMovementSpeed(CallbackInfoReturnable<Float> cir) {
+        if(((LivingEntity) (Object) this) instanceof AbstractHorseEntity horseEntity) {
+            System.out.println("a");
+            if(horseEntity.getControllingPassenger() instanceof PlayerEntity player) {
+                System.out.println("b");
+                Wrapper<Float> increase = Wrapper.of(1f);
+                ArmorTrimEffects.REDSTONE.apply(player.getArmorItems(), stack -> {
+                    System.out.println("c");
+                    increase.set(increase.get() + Config.getInstance().redstoneMovementSpeedIncrease);
+                });
+                cir.setReturnValue(cir.getReturnValue() * increase.get());
+            }
+        }
         Wrapper<Float> increase = Wrapper.of(1f);
         ArmorTrimEffects.REDSTONE.apply(getArmorItems(), stack -> increase.set(increase.get() + Config.getInstance().redstoneMovementSpeedIncrease));
         cir.setReturnValue(cir.getReturnValue() * increase.get());
+    }
+
+    @ModifyExpressionValue(method = "travel", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;getVelocity()Lnet/minecraft/util/math/Vec3d;", ordinal = 2), slice =
+    @Slice(
+            from = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;isFallFlying()Z"),
+            to = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;getWorld()Lnet/minecraft/world/World;", ordinal = 1)
+    ))
+    private Vec3d modifyFlightSpeed(Vec3d original) {
+        Wrapper<Float> increase = Wrapper.of(1f);
+        ArmorTrimEffects.REDSTONE.apply(getArmorItems(), stack -> increase.set(increase.get() + Config.getInstance().redstoneMovementSpeedIncrease));
+        return original.multiply(increase.get(), 1, increase.get());
     }
 
     @WrapOperation(method = "applyArmorToDamage", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/DamageUtil;getDamageLeft(FFF)F"))
